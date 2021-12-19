34251b0f94a739280a47bddfbc5ae7a82fa2575b
NancyLi0637 <nancyl001237@gmail.com>
2021-12-18 19:54:47 -0500

    added examples and api route

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/pub/landingPage.html'))
 })
 
-app.get('/examples', (req, res) => {
+app.get('/examples-cyclit', (req, res) => {
     res.sendFile(path.join(__dirname, '/pub/examples.html'))
 })
+
+app.get('/api-cyclit', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pub/api.html'))
+})
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
